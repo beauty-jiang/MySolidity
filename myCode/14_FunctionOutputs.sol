6ff44c38040cbbe4f8b8d96a50f827dae4e62643
jiangyu <2389392101@qq.com>
2024-11-13 19:49:38 +0800

    14_函数返回值

--- myCode/14_FunctionOutputs.sol
+++ myCode/14_FunctionOutputs.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.7;
 // 函数返回值
 contract FunctionOutputs {
     function returnMany() public pure returns(uint,bool){//公开函数 内部外包都可以调用
-        return(1,true)
+        return(1,true);
     } 
     // 返回值加上名字
     function named() public pure returns (uint x,bool b){
-        return(1,true)
+        return(1,true);
     }
     // 隐式返回
     function assigned() public pure returns(uint x,bool y){
