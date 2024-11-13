1d70c9d53d2861ff7d599e467289fa28974e9bc1
jiangyu <2389392101@qq.com>
2024-11-13 16:55:43 +0800

    09_循环

--- myCode/09_ForAndWhileLoops.sol
+++ myCode/09_ForAndWhileLoops.sol
@@ -8,4 +8,13 @@ contract ForAndWhileLoops {
             if(i == 3){ continue }
         }
     }
+
+    // 在智能合约中要控制循环的数量 太多gas费会多
+    function sum(uint n) external pure returns(uint){
+        uint s;
+        for (uint i = 1; i <= _n; i++) {
+            s += i;
+        }
+        return s;
+    }
 }
