0eefd322d60adccafe36a3f987f4ad58330c9603
jiangyu <2389392101@qq.com>
2024-11-13 19:45:33 +0800

    14_函数返回值

---
+++ myCode/14_FunctionOutputs.sol
@@ -0,0 +1,25 @@
+ // SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.7;
+
+// 函数返回值
+contract FunctionOutputs {
+    function returnMany() public pure returns(uint,bool){//公开函数 内部外包都可以调用
+        return(1,true)
+    } 
+    // 返回值加上名字
+    function named() public pure returns (uint x,bool b){
+        return(1,true)
+    }
+    // 隐式返回
+    function assigned() public pure returns(uint x,bool y){
+        x=1;
+        b=true;
+    }
+
+// 如何接收返回值？
+    function destructingAssigments() public pure{
+        (uint x,bool b) = returnMany();
+        // 假如只用一个呢？
+        (,bool b) = returnMany();
+    }
+}
--- myCode/README.md
+++ myCode/README.md
@@ -11,3 +11,4 @@
 11_FunctionModifer 函数修改器
 12_Constructor 构造函数
 13_Owanable 小总结 Owanable合约
+14_FunctionOutputs 函数返回值
